798fadbd6767b3745d76106e781b2d664120eb8d
Minhtuan622 <minhtuannguyen22062001@gmail.com>
2022-11-07 22:18:57 +0700

    create components of public

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -4,11 +4,35 @@ import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
 import {SharedModule} from "../shared/shared.module";
+import { HeaderComponent } from './components/home/header/header.component';
+import { ContentComponent } from './components/home/content/content.component';
+import { FooterComponent } from './components/home/footer/footer.component';
+import { GameItemComponent } from './components/home/listGame/game-item/game-item.component';
+import { ListGameComponent } from './components/home/listGame/list-game.component';
+import { ReactComponent } from './components/home/listGame/react/react.component';
+import { FilterComponent } from './components/home/listGame/filter/filter.component';
+import { HomeComponent } from './components/home/home.component';
+import { GameDetailComponent } from './components/game-detail/game-detail.component';
+import { GameInfoComponent } from './components/game-detail/game-info/game-info.component';
+import { RelatedGameComponent } from './components/game-detail/related-game/related-game.component';
+import { HowToPlayComponent } from './components/game-detail/how-to-play/how-to-play.component';
 
 
 @NgModule({
   declarations: [
-    PublicComponent
+    PublicComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    GameItemComponent,
+    ListGameComponent,
+    ReactComponent,
+    FilterComponent,
+    HomeComponent,
+    GameDetailComponent,
+    GameInfoComponent,
+    RelatedGameComponent,
+    HowToPlayComponent
   ],
   imports: [
     CommonModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './components/button/button.component';
+import { PaginatorComponent } from './components/paginator/paginator.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ButtonComponent,
+    PaginatorComponent,
+    SearchComponent
+  ],
   imports: [
     CommonModule
   ]
