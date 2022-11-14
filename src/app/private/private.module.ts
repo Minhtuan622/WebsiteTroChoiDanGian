77a41f50948e3702ee8f1c20d8f0690e2471743d
dev-to2k <trungnek175@gmail.com>
2022-11-14 19:20:27 +0700

    setup components and libraries

--- src/app/private/private.module.ts
+++ src/app/private/private.module.ts
@@ -1,21 +1,29 @@
-import { NgModule } from '@angular/core';
+// Modules
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from "../shared/shared.module";
 
+// Components
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { PrivateRoutingModule } from './private-routing.module';
 import { PrivateComponent } from './private.component';
-import {SharedModule} from "../shared/shared.module";
-import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     PrivateComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,
     PrivateRoutingModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ]
 })
 export class PrivateModule { }
